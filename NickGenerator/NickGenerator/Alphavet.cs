ed9cb6c88a40581ed6ceb6749e27873c814d8848
Olex <Olex@TERRA>
2018-02-07 02:25:11 +0200

    first commit

---
+++ NickGenerator/NickGenerator/Alphavet.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NickGenerator
+{
+   class Alphavet
+    {
+       Random rnd = new Random();
+       char[] Golosny = { 'a','e','y','u','i','o'};
+       char[] Prigolosny = { 'q', 'w', 'r', 'b', 'n', 'm', 'p', 'z', 'x', 'c', 'd', 'f', 'j', 'k', 'l', 't', 's', 'f'};
+       
+       public char GetG()
+        {
+            
+            int a =rnd.Next(1,Golosny.Length);
+            return(Golosny[a]);
+        }
+
+        public char GetP()
+        {
+
+            int a = rnd.Next(1, Prigolosny.Length);
+            return (Prigolosny[a]);
+        }
+    }
+
+}
---
+++ NickGenerator/NickGenerator/Nick.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+
+namespace NickGenerator
+{
+    class Nick
+    {
+        StringBuilder Generate_Nick()
+        {
+            StringBuilder rezult = new StringBuilder("");
+            Random rnd = new Random();
+            Alphavet T = new Alphavet();
+
+
+            
+                for (int i = 0; i < rnd.Next(3, 11); i++)
+                {
+
+                if (i % 3 == 0)
+                    rezult.Append(T.GetP());
+                else
+                    rezult.Append(T.GetG());
+                }
+               
+            
+            return (rezult);
+        }
+    }
+}
